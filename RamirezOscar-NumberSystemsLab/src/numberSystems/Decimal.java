1d423d5a00712a315fb9516dba58bcd85c44ad65
orami <oscarjoramirez@yahoo.com>
2020-01-31 15:57:28 -0800

    add decimal to hex conversion. refactor methods

--- RamirezOscar-NumberSystemsLab/src/numberSystems/Decimal.java
+++ RamirezOscar-NumberSystemsLab/src/numberSystems/Decimal.java
@@ -10,15 +10,18 @@ import java.util.Scanner;
  * @author Oscar Ramirez
  * @version 1/30/20
  *
- */
+ */   
 public class Decimal
 {
     private PrintWriter out;
     private Scanner in;
     private int dec;
     private StringBuilder bin;
-    private int nibble;
     private StringBuilder hex;
+    private final int HEX_SIZE = 8;
+    private final int BIN_FLAG = 0;
+    private final int HEX_FLAG = 1;
+    private final int NIBBLE_SIZE = 4;
     
     public Decimal(PrintWriter out)
     {
@@ -26,16 +29,23 @@ public class Decimal
         in = new Scanner(System.in);
         dec = -1;
         bin = new StringBuilder(Integer.SIZE);
-        nibble = 4;
-        hex = new StringBuilder(nibble);
+        hex = new StringBuilder(HEX_SIZE);
     }
     
     public void decToBin()
     {
         inputDec();
-        toBin();
-        formatBin();
-        displayBin();
+        convert(BIN_FLAG);
+        format(BIN_FLAG);
+        display(BIN_FLAG);
+    }
+
+    public void decToHex()
+    {
+        inputDec();
+        convert(HEX_FLAG);
+        format(HEX_FLAG);
+        display(HEX_FLAG);
     }
 
     private void inputDec()
@@ -86,59 +96,114 @@ public class Decimal
         out.println(errorMessage);
     }
     
-    private void toBin()
+    private void convert(int flag)
     {
-        int value = dec;
-        bin = new StringBuilder(Integer.SIZE);
-        while (value != 0) {
-            bin.append(value % 2);
-            value /= 2;
+        if (flag == BIN_FLAG) {
+            int value = dec;
+            reset(flag);
+            while (value != 0) {
+                bin.append(value % 2);
+                value /= 2;
+            }
+        }
+        else {
+            int value = dec;
+            reset(flag);
+            int hexBase = 16;
+            while (value != 0) {
+                hex.append(nextHexDigit(value));
+                value /= hexBase;
+            }
+        }
+    }
+
+    private void reset(int flag)
+    {
+        if (flag == BIN_FLAG) {
+            bin = new StringBuilder(Integer.SIZE);
+        }
+        else {
+            hex = new StringBuilder(HEX_SIZE);
         }
     }
     
-    private void formatBin()
-    {
-        fillBin();
-        bin = bin.reverse();
-        addSpaces();
+    private String nextHexDigit(int hex) {
+        String nextHexDigit = "";
+        switch (hex % 16) {
+        case 1 : nextHexDigit = "1"; break;
+        case 2 : nextHexDigit = "2"; break;
+        case 3 : nextHexDigit = "3"; break;
+        case 4 : nextHexDigit = "4"; break;
+        case 5 : nextHexDigit = "5"; break;
+        case 6 : nextHexDigit = "6"; break;
+        case 7 : nextHexDigit = "7"; break;
+        case 8 : nextHexDigit = "8"; break;
+        case 9 : nextHexDigit = "9"; break;
+        case 10 : nextHexDigit = "A"; break;
+        case 11 : nextHexDigit = "B"; break;
+        case 12 : nextHexDigit = "C"; break;
+        case 13 : nextHexDigit = "D"; break;
+        case 14 : nextHexDigit = "E"; break;
+        case 15 : nextHexDigit = "F"; break;
+        }
+        return nextHexDigit;
     }
     
-    private void fillBin()
+    private void format(int flag)
     {
-        while(bin.length() < Integer.SIZE) {
-            bin.append(0);
+        if (flag == BIN_FLAG) {
+            fill(BIN_FLAG);
+            bin = bin.reverse();
+            addSpaces();
+        }
+        else {
+            fill(HEX_FLAG);
+            hex = hex.reverse();
+            addFlag();
         }
     }
     
+    private void fill(int flag)
+    {
+        if (flag == BIN_FLAG) {
+            while(bin.length() < Integer.SIZE) {
+                bin.append(0);
+            }
+        }
+        else {
+            while(hex.length() < HEX_SIZE) {
+                hex.append(0);
+            }
+        }
+    }
+
     private void addSpaces()
     {
-        for (int i = bin.length() - nibble; i >= nibble; i -= nibble) {
+        for (int i = bin.length() - NIBBLE_SIZE; i >= NIBBLE_SIZE;
+                i -= NIBBLE_SIZE) {
             bin.insert(i, " ");
         }
     }
     
-    private void displayBin()
+    private void addFlag()
     {
-        String displayMessage = String.format(
-                "%d converted to binary is: %s%n", dec, bin.toString());
+        hex.insert(0, "0x");
+    }
+    
+    private void display(int flag)
+    {
+        String displayMessage;
+        if (flag == BIN_FLAG) {
+            displayMessage = String.format(
+                    "%d converted to binary is: %s%n",
+                    dec, bin.toString());
+        }
+        else {
+            displayMessage = String.format(
+                    "%d converted to hexadecimal is: %s%n",
+                    dec, hex.toString());
+        }
         System.out.println(displayMessage);
         out.println(displayMessage);
-    }
-    
-    public void decToHex()
-    {
-        inputDec();
-        toHex();
-        outHex();
-    }
-    
-    private void toHex()
-    {
-        
-    }
-    
-    private void outHex()
-    {
-        
     }
 }
